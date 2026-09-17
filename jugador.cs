eff9da820a0d249af1da9ae67236b4d14a889053
alexwuuu15-hub <alexwuuu15@gmail.com>
2026-09-17 16:41:53 -0400

    Create jugador.cs

---
+++ jugador.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+#region Jugador
+
+namespace Juego.Personajes
+{
+    public partial class Jugador
+    {
+        public void Caminar()
+        {
+        }
+    }
+}
+
+namespace Juego.Personajes
+{
+    public partial class Jugador
+    {
+        public void Saltar()
+        {
+        }
+    }
+}
+
+#endregion
+
+
+#region Enemigos
+
+namespace Juego.Enemigos
+{
+    public class Enemigo
+    {
+        public void Atacar()
+        {
+        }
+    }
+}
+
+#endregion
+
+
+#region Objetos
+
+namespace Juego.Objetos
+{
+    public class Moneda
+    {
+        public void Recoger()
+        {
+        }
+    }
+}
+
+#endregion
